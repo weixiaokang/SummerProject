36a61da04a63e62864c9f25d42887bc79f31fcdb
weixiaokang <kang21472382@qq.com>
2014-08-19 22:50:41 +0800

    using android studio 'github for just test

--- app3/src/main/java/com/weixiaokang/threepointchess/MainActivity.java
+++ app3/src/main/java/com/weixiaokang/threepointchess/MainActivity.java
@@ -8,7 +8,7 @@ import android.view.MenuItem;
 
 public class MainActivity extends ActionBarActivity {
 
-    private String TEXT = "just test";
+    private String TEXT = "just test 2";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
