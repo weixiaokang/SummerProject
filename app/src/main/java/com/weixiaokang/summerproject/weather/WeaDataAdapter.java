9cab61c529b0fc6a938b11c41192910328da30b9
weixiaokang <kang21472382@qq.com>
2014-09-03 23:52:14 +0800

    today nothing to do

--- app/src/main/java/com/weixiaokang/summerproject/weather/WeaDataAdapter.java
+++ app/src/main/java/com/weixiaokang/summerproject/weather/WeaDataAdapter.java
@@ -1,36 +1,42 @@
 package com.weixiaokang.summerproject.weather;
 
 import android.content.Context;
-import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.TextView;
 
 import com.weixiaokang.summerproject.R;
+
+import java.util.LinkedList;
 
 /**
  * Created by Administrator on 2014/9/2.
  */
 public class WeaDataAdapter extends BaseAdapter {
 
-    Context context;
-    public WeaDataAdapter(Context context) {
+    private Context context;
+    private LinkedList<String> data;
+    private View views[];
+    public WeaDataAdapter(Context context, LinkedList<String> data) {
         this.context = context;
+        this.data = data;
+        views = new View[data.size()];
     }
     @Override
     public int getCount() {
-        return 0;
+        return data.size();
     }
 
     @Override
     public Object getItem(int position) {
-        return null;
+        return views[position];
     }
 
     @Override
     public long getItemId(int position) {
-        return 0;
+        return position;
     }
 
     @Override
@@ -39,6 +45,13 @@ public class WeaDataAdapter extends BaseAdapter {
         if (convertView == null) {
             LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             view = layoutInflater.inflate(R.layout.view_list, null);
+            TextView weekView = (TextView) view.findViewById(R.id.week_view);
+            TextView weatherView = (TextView) view.findViewById(R.id.weather_view);
+            TextView tempView = (TextView) view.findViewById(R.id.temp_view);
+            weekView.setText(data.get(position * 3));
+            weatherView.setText(data.get(position * 3 + 1));
+            tempView.setText(data.get(position * 3 + 2));
+            views[position] = view;
         } else {
             view = convertView;
         }
